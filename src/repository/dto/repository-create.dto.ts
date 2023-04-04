5e7d4a5e051dd0f215fbacd8a5fb5eeff8444a6a
Ivan Levchenko <ivan.levchenko@plus4u.net>
2023-04-04 14:31:44 +0300

    fix: added name prop to repository-create.dto & id prop to repository.entity

--- src/repository/dto/repository-create.dto.ts
+++ src/repository/dto/repository-create.dto.ts
@@ -2,9 +2,12 @@ import { ArrayMinSize, IsArray, IsString } from "class-validator";
 
 export abstract class RepositoryCreateDto {
   @IsString()
+  name: string;
+
+  @IsString()
   subject: string;
 
-  @IsArray({ each: true })
+  @IsArray()
   @ArrayMinSize(1)
   groups: string[];
 }
