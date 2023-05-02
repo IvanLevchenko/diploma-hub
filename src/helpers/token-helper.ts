44bddf1a48316c87e09d7b812cc64a116aa4c227
Ivan Levchenko <ivanlion1301@gmail.com>
2023-05-02 17:41:22 +0300

    fix: in token helper changed payload for refresh token, added decodeToken & isTokenValid methods

--- src/helpers/token-helper.ts
+++ src/helpers/token-helper.ts
@@ -1,3 +1,4 @@
+import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 
 import { User } from "../user/user.entity";
@@ -5,13 +6,14 @@ import { AuthorizationResult } from "../common/types/authorization-result";
 import { UserService } from "../user/user.service";
 import { TokenPayload } from "../common/interfaces/token-payload";
 
+@Injectable()
 class TokenHelper {
   constructor(
     private jwtService: JwtService,
     private userService: UserService,
   ) {}
 
-  async generateTokens(user: User): Promise<AuthorizationResult> {
+  public async generateTokens(user: User): Promise<AuthorizationResult> {
     const payload: TokenPayload = {
       id: user.id,
       email: user.email,
@@ -22,10 +24,15 @@ class TokenHelper {
 
     const token = this.jwtService.sign(payload, {
       secret: process.env.SECRET_KEY,
+      expiresIn: "1h",
     });
-    const refreshToken = this.jwtService.sign(payload, {
-      secret: process.env.SECRET_REFRESH_KEY,
-    });
+    const refreshToken = this.jwtService.sign(
+      { id: user.id },
+      {
+        secret: process.env.SECRET_REFRESH_KEY,
+        expiresIn: "3d",
+      },
+    );
 
     await this.userService.update({
       id: user.id,
@@ -38,8 +45,28 @@ class TokenHelper {
     };
   }
 
-  decodeToken(token: string): TokenPayload {
-    return this.jwtService.decode(token.split(" ")[1]) as TokenPayload;
+  private removeBearer(token: string): string {
+    if (token.includes("Bearer")) {
+      token = token.split(" ")[1];
+    }
+
+    return token;
+  }
+
+  public decodeToken(token: string): TokenPayload {
+    token = this.removeBearer(token);
+    return this.jwtService.decode(token) as TokenPayload;
+  }
+
+  public isTokenValid(token: string): boolean {
+    token = this.removeBearer(token);
+    try {
+      this.jwtService.verify(token, { secret: process.env.SECRET_KEY });
+    } catch (e) {
+      return false;
+    }
+
+    return true;
   }
 }
 
