8e34ae35cff1a64cc490b8cd794bdc61001a1c57
Ivan Levchenko <ivanlion1301@gmail.com>
2023-05-23 15:11:00 +0300

    fix: added export for file creat dto

--- src/file/dto/index.ts
+++ src/file/dto/index.ts
@@ -2,3 +2,4 @@ export { FileCreateDto } from "./file-create.dto";
 export { FileListDto } from "./file-list.dto";
 export { FileGetDto } from "./file-get.dto";
 export { FileDeleteDto } from "./file-delete.dto";
+export { FileCreateDtoOut } from "./file-create.dto";
